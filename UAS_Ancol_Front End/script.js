8dd5ad8a1cc90773250efd76c88db597eb6fc8fd
Revixyy <revixy.ofc@gmail.com>
2023-12-05 10:49:44 +0700

    membuat fungsi tombol more destination

--- UAS_Ancol_Front End/script.js
+++ UAS_Ancol_Front End/script.js
@@ -44,3 +44,73 @@ window.addEventListener("scroll", function () {
   }
 
 });
+
+// YANG INI BIARIN AJA DULU TAKUTNYA YANG BAWAH ERROR MUACH
+// Load More Button
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const loadMoreBtn = document.getElementById('load-more');
+//   let currentItem = 3;
+
+//   loadMoreBtn.addEventListener('click', () => {
+//     const elementList = [...document.querySelectorAll('.popular-list li')];
+    
+//     for (let i = currentItem; i < currentItem + 3 && i < elementList.length; i++) {
+//       elementList[i].style.display = 'flex';
+//     }
+    
+//     currentItem += 3;
+
+//     if (currentItem >= elementList.length) {
+//       loadMoreBtn.style.display = 'none';
+//     }
+//   });
+// });
+// MUACH
+
+//show less button
+document.addEventListener('DOMContentLoaded', function () {
+  const loadMoreBtn = document.getElementById('load-more');
+  const showLessBtn = document.getElementById('show-less');
+  let currentItem = 3;
+
+  // Function to hide all elements
+  function hideAllElements() {
+    const elementList = document.querySelectorAll('.popular-list li');
+    elementList.forEach((element, index) => {
+      if (index >= currentItem) {
+        element.style.display = 'none';
+      } else {
+        element.style.display = 'flex';
+      }
+    });
+  }
+
+  // Initial hide for all elements except the first 3
+  hideAllElements();
+
+  // Event listener for Load More button
+  loadMoreBtn.addEventListener('click', () => {
+    const elementList = document.querySelectorAll('.popular-list li');
+    
+    for (let i = currentItem; i < currentItem + 3 && i < elementList.length; i++) {
+      elementList[i].style.display = 'flex';
+    }
+    
+    currentItem += 3;
+
+    if (currentItem >= elementList.length) {
+      loadMoreBtn.style.display = 'none';
+      showLessBtn.style.display = 'flex'; // Show the "Show Less" button
+    }
+  });
+
+  // Event listener for Show Less button
+  showLessBtn.addEventListener('click', () => {
+    currentItem = 3; // Reset the currentItem to 3
+    hideAllElements(); // Hide all elements
+    loadMoreBtn.style.display = 'flex'; // Show the "Load More" button
+    showLessBtn.style.display = 'none'; // Hide the "Show Less" button
+  });
+});
+
