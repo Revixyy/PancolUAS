40cea56bc6ee800d31b2cdf7a8413e0698a8c98c
Daflah <daflah.t.gusra@gmail.com>
2023-12-06 18:18:54 +0700

    Twelve Commit Ngab

--- UAS_Ancol_Front End/script.js
+++ UAS_Ancol_Front End/script.js
@@ -172,3 +172,40 @@ app.controller('promoCtrl', function($scope) {
   $scope.promoLink = 'https://www.ancol.com/promo';
   $scope.promoButton = 'Redeem Now !!';
 });
+
+//carousel
+var slideIndex = 1;
+        showSlide(slideIndex);
+
+        function nextSlide(n){
+            showSlide(slideIndex += n);
+        }
+
+        function dotSlide(n){
+            showSlide(slideIndex = n);
+        }
+
+        function showSlide(n){
+            var i;
+            var slides = document.getElementsByClassName("imgslide");
+            var dots = document.getElementsByClassName("dot");
+
+            if (n > slides.length){
+                slideIndex = 1;
+            }
+
+            if (n < 1){
+                slideIndex = slides.length;
+            }
+
+            for (i = 0; i < slides.length; i++){
+                slides[i].style.display = "none";
+            }
+
+            for (i = 0; i < dots.length; i++){
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " active";
+        }
